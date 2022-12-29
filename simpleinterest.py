ebe54aa78cf3930ffc845a73e94c568c6654184e
kriti upadhyay <kritiu54@gmail.com>
2022-12-29 21:57:57 +0545

    project start

---
+++ simpleinterest.py
@@ -0,0 +1,9 @@
+def simple_interest(principal, rate, time):
+  return (principal * rate * time) / 100
+
+principal = 10000
+rate = 7.5
+time = 5
+
+interest = simple_interest(principal, rate, time)
+print(f"Simple interest: {interest}")
